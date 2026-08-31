04d34924ce31b9e1202b5d22dad1ee9d00175a47
João Victor Santos <joaovictormonteirosantos6@gmail.com>
2026-08-31 00:56:56 -0300

    feat: coleta e apresenta dados do aluno

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -1,1 +1,7 @@
+const nome = prompt("Qual é o seu nome?")
+const idade = prompt("Quantos anos você tem?")
+const curso = prompt("Qual é o nome do seu curso?")
+const cidade = prompt("Onde você mora?")
 
+console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e estou cursando ${curso}`)
+console.log("Olá, meu nome é " + nome + ", tenho " + idade + " anos, moro em " + cidade + " e estou cursando " + curso)
